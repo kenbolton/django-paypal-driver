c9b8b26f627f0714a6f75227f753bd9d2466a663
Ken Bolton <ken@bscientific.org>
2011-11-10 16:09:11 -0500

    Fix direct payment bugs, add documentation.

--- paypal/admin.py
+++ paypal/admin.py
@@ -4,16 +4,16 @@ from django.contrib import admin
 from paypal.models import PayPalResponse, PayPalResponseStatus
 
 class PayPalResponseAdmin(admin.ModelAdmin):
-    list_display = ('token',
+    list_display = (
                     'trans_id',
+                    'status',
+                    'token',
                     'currencycode',
-                    'error_msg',
                     'charged',
                     'payment_received',
-                    'status')
+                    'error_msg',
+                    )
     list_filter = ("payment_received", "status")
-    
     search_fields = ('token', 'trans_id')
 
 admin.site.register(PayPalResponse, PayPalResponseAdmin)
-admin.site.register(PayPalResponseStatus)
--- paypal/models.py
+++ paypal/models.py
@@ -17,8 +17,8 @@ class PayPalResponseStatus(models.Model):
 
 
 class PayPalResponse(models.Model):
-    token = models.CharField(max_length = 256, null = True, blank = True, db_index = True)
     trans_id = models.CharField(max_length = 256, null = True, blank = True, db_index = True)
+    token = models.CharField(max_length = 256, null = True, blank = True, db_index = True)
     correlation_id = models.CharField(max_length = 256, null = True, blank = True)
     response = models.CharField(max_length = 32)
     error_msg = models.CharField(max_length = 256, null = True, blank = True)
@@ -32,56 +32,71 @@ class PayPalResponse(models.Model):
     class Meta:
         verbose_name = _("PayPal Response")
         verbose_name_plural = _("PayPal Responses")
-        
+
     def __unicode__(self):
         return self.token or u''
-        
+
     @staticmethod
     def get_default_status():
-        ps, created = PayPalResponseStatus.objects.get_or_create(summary = _("Sale"))
+        ps, created = PayPalResponseStatus.objects.get_or_create(summary=_("Sale"))
         return ps
 
 
     @staticmethod
     def get_cancel_status():
-        ps, created = PayPalResponseStatus.objects.get_or_create(summary = _("Refund"))
+        ps, created = PayPalResponseStatus.objects.get_or_create(summary=_("Refund"))
         return ps
 
-
-    def fill_from_response(self, response, action = "Sale"):
+    def fill_from_response(self, response, action="Sale"):
         """
         For payment:
-        
-        {'ORDERTIME': '2009-12-13T07:16:03Z', 'ACK': 'Success', 'TIMESTAMP': '2009-12-13T07:16:04Z', 'CURRENCYCODE': 'AUD',
-        'PAYMENTSTATUS': 'Pending', 'PENDINGREASON': 'multicurrency', 'PAYMENTTYPE': 'instant', 'TOKEN': 'EC-3AX26844Y52324328',
-        'VERSION': '53.0', 'BUILD': '1105502', 'TAXAMT': '0.00', 'REASONCODE': 'None', 'TRANSACTIONID': '9XL27840ED344594X',
-        'AMT': '13.89', 'CORRELATIONID': 'f13dc648551be', 'TRANSACTIONTYPE': 'expresscheckout'}
+
+        {'ORDERTIME': '2009-12-13T07:16:03Z', 'ACK': 'Success',
+        'TIMESTAMP': '2009-12-13T07:16:04Z', 'CURRENCYCODE': 'AUD',
+        'PAYMENTSTATUS': 'Pending', 'PENDINGREASON': 'multicurrency',
+        'PAYMENTTYPE': 'instant', 'TOKEN': 'EC-3AX26844Y52324328',
+        'VERSION': '53.0', 'BUILD': '1105502', 'TAXAMT': '0.00',
+        'REASONCODE': 'None', 'TRANSACTIONID': '9XL27840ED344594X',
+        'AMT': '13.89', 'CORRELATIONID': 'f13dc648551be',
+        'TRANSACTIONTYPE': 'expresscheckout'}
 
         or
 
-        {'ACK': 'Failure', 'TIMESTAMP': '2009-12-13T07:43:00Z', 'L_SEVERITYCODE0': 'Error', 'L_SHORTMESSAGE0': 'Invalid token',
-        'L_LONGMESSAGE0': 'Invalid token.', 'VERSION': '53.0', 'BUILD': '1105502', 'L_ERRORCODE0': '10410', 'CORRELATIONID': 'c3e201df48a42'}
-
+        {'ACK': 'Failure', 'TIMESTAMP': '2009-12-13T07:43:00Z',
+        'L_SEVERITYCODE0': 'Error', 'L_SHORTMESSAGE0': 'Invalid token',
+        'L_LONGMESSAGE0': 'Invalid token.', 'VERSION': '53.0',
+        'BUILD': '1105502', 'L_ERRORCODE0': '10410',
+        'CORRELATIONID': 'c3e201df48a42'}
 
         --------------------------
 
-        
         For refund:
 
-        {'ACK': 'Failure', 'TIMESTAMP': '2009-12-13T09:51:19Z', 'L_SEVERITYCODE0': 'Error', 'L_SHORTMESSAGE0':
-         'Permission denied', 'L_LONGMESSAGE0': 'You do not have permission to refund this transaction', 'VERSION': '53.0',
-         'BUILD': '1077585', 'L_ERRORCODE0': '10007', 'CORRELATIONID': '3d8fa24c46c65'}
+        {'ACK': 'Failure', 'TIMESTAMP': '2009-12-13T09:51:19Z',
+        'L_SEVERITYCODE0': 'Error', 'L_SHORTMESSAGE0': 'Permission denied',
+        'L_LONGMESSAGE0': 'You do not have permission to refund this transaction',
+        'VERSION': '53.0', 'BUILD': '1077585', 'L_ERRORCODE0': '10007',
+        'CORRELATIONID': '3d8fa24c46c65'}
 
          or
-    
+
          {'REFUNDTRANSACTIONID': '9E679139T5135712L', 'FEEREFUNDAMT': '0.70', 'ACK': 'Success', 'TIMESTAMP': '2009-12-13T09:53:06Z',
          'CURRENCYCODE': 'AUD', 'GROSSREFUNDAMT': '13.89', 'VERSION': '53.0', 'BUILD': '1077585', 'NETREFUNDAMT': '13.19',
          'CORRELATIONID': '6c95d7f979fc1'}
 
+        --------------------------
+
+        For direct payment:
+
+        {'ACK': 'Success', 'TIMESTAMP': '2011-11-10T17:04:31Z',
+        'CURRENCYCODE': 'USD', 'VERSION': '53.0', 'BUILD': '2230381',
+        'TRANSACTIONID': '2N691362SB560793C', 'AVSCODE': 'X',
+        'AMT': '60.00', 'CORRELATIONID': 'e8c212627e9ab',
+        'CVV2MATCH': 'M'  }
         """
 
         def get_value_from_qs(qs, value):
-            raw = qs.get(value)
+            raw = qs().get(value)
             if type(raw) == list:
                 try:
                     return raw[0]
@@ -109,3 +124,13 @@ class PayPalResponse(models.Model):
             self.error_msg = get_value_from_qs(response, "L_SHORTMESSAGE0")
             self.currencycode = get_value_from_qs(response, "CURRENCYCODE")
             self.raw_response = str(response)
+        elif action == "Payment":
+            self.trans_id = get_value_from_qs(response, "TRANSACTIONID")
+            self.response = get_value_from_qs(response, "ACK")
+            self.raw_response = str(response())
+            amount = get_value_from_qs(response, "AMT")
+            self.charged = Decimal(amount) if amount is not None else None
+            self.correlation_id = get_value_from_qs(response, "CORRELATIONID")
+            self.currencycode = get_value_from_qs(response, "CURRENCYCODE")
+            self.error_msg = get_value_from_qs(response, "L_SHORTMESSAGE0")	
+            self.payment_received = True
